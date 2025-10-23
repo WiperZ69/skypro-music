fa6a1d5e09c3fe109298f55d22c731de2df6ae54
WiperZ69 <yura.4etin.03@gmail.com>
2025-10-23 20:17:19 +0800

    remove protectedRoute

--- src/app/music/layout.tsx
+++ src/app/music/layout.tsx
@@ -5,25 +5,22 @@ import FetchingTracks from '../../components/FetchingTracks/FetchingTracks'
 import { Nav } from '../../components/Nav/Nav'
 import { Sidebar } from '../../components/Sidebar/Sidebar'
 import { useInitAuth } from '../hooks/useInitAuth'
-import { ProtectedRoute } from '../ProtectedRoute'
 import styles from './layout.module.scss'
 
 export default function MusicPage({ children }: { children: React.ReactNode }) {
 	useInitAuth()
 	return (
-		<ProtectedRoute>
-			<div className={styles.wrapper}>
-				<div className={styles.container}>
-					<main className={styles.main}>
-						<FetchingTracks />
-						<Nav />
-						{children}
-						<Sidebar />
-					</main>
-					<Bar />
-					<footer className='footer'></footer>
-				</div>
+		<div className={styles.wrapper}>
+			<div className={styles.container}>
+				<main className={styles.main}>
+					<FetchingTracks />
+					<Nav />
+					{children}
+					<Sidebar />
+				</main>
+				<Bar />
+				<footer className='footer'></footer>
 			</div>
-		</ProtectedRoute>
+		</div>
 	)
 }
